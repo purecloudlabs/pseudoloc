50e557fa1961a0154dfcffc002e467ae7962d04b
Carlton Whitehead <carlton.whitehead@inin.com>
2017-06-15 09:58:06 -0400

    Add an integration test with a Genesys slogan

--- core/src/test/kotlin/com/genesys/pseudoloc/it/GeneratorIntegrationTest.kt
+++ core/src/test/kotlin/com/genesys/pseudoloc/it/GeneratorIntegrationTest.kt
@@ -2,6 +2,7 @@ package com.genesys.pseudoloc.it
 
 import com.genesys.pseudoloc.Generator
 import com.genesys.pseudoloc.util.INPUT_VERY_SHORT
+import org.assertj.core.api.Assertions
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -20,6 +21,16 @@ class GeneratorIntegrationTest {
         val expected = "[Nō]"
 
         val actual = generator.generate(INPUT_VERY_SHORT)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun itShouldGenerateFromGenesysSlogan() {
+        val expected = "[‘İ球Gēńęśyş ĩś Trũştěd by Ovēr 10,000 Cōmpåņĭęś Glŏbâlly___________яش]"
+        val input = "Genesys is Trusted by Over 10,000 Companies Globally"
+
+        val actual = generator.generate(input)
 
         assertEquals(expected, actual)
     }
